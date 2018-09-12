027fd27f24237e8b0bb2937a25b1b2dad856ae9c
Robert Charlton <robertcharlon23@outlook.com>
2018-09-12 06:53:19 +0000

    .

--- check_login.php
+++ check_login.php
@@ -10,11 +10,12 @@
 // Lumberjacks Incorperated (2018)
 //--------------------------------------------------------------------------------------------------------------
 
+// Header needed by REACT
+header("Access-control-allow-origin: *");
+
 //---------------------------------------- 
 // INCLUDES
 //---------------------------------------- 
-#include_once dirname(__FILE__).'/../_dependencies/core_procedures/recieved_message_storage_php_api.php';
-#include_once dirname(__FILE__).'/../_dependencies/php_environment_php_api.php';
 include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
 
 //---------------------------------------- 
