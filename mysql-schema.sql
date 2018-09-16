3d0cd987a9726c4b689f8104f3709e856225428a
Daniel Kennedy <dannykennedy@email.com>
2018-09-17 09:26:29 +1000

    new schema

--- mysql-schema.sql
+++ mysql-schema.sql
@@ -1,22 +1,58 @@
 
--- TAGNOSTIC DB SCHEMA V2
+-- TAGNOSTIC DB SCHEMA V3
 
-CREATE TABLE Users (
-id VARCHAR(36) PRIMARY KEY,
-firstName VARCHAR(40) NOT NULL,
-lastName VARCHAR(40),
-email VARCHAR(80) UNIQUE NOT NULL
-);
+DROP TABLE IF EXISTS `Accounts`;
+CREATE TABLE `Accounts` (
+  `account_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password_sha1` varchar(64) NOT NULL,
+  firstName VARCHAR(40),
+  lastName VARCHAR(40),
+  email VARCHAR(80) UNIQUE,
+  PRIMARY KEY (`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- Insert some dummy accounts
+LOCK TABLES `Accounts` WRITE;
+INSERT INTO `Accounts` VALUES (1,'Bob','9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684', 'Bob', 'Smith', 'b@s.com');
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `Sessions`;
+CREATE TABLE `Sessions` (
+  `session_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` int(10) unsigned NOT NULL,
+  `session_key_sha1` varchar(64) NOT NULL,
+  `last_session_renewal` datetime NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `account_id` (`account_id`),
+  CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `Accounts` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=140 DEFAULT CHARSET=utf8mb4;
+
+LOCK TABLES `Sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+INSERT INTO `Sessions` VALUES (1,1,'5BAA61E4C9B93F3F0682250B6CF8331B7EE68FD8','2018-08-28 22:35:23');
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --Basically the text blobs that people add. 
 --This is a simple design for now.
-CREATE TABLE Items (
-id VARCHAR(36) PRIMARY KEY,
-entryText TEXT,
-userID VARCHAR(36) references Users(id),
-timePosted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-timeModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+  `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `account_id` int(10) unsigned NOT NULL,
+  `item_text` text NOT NULL,
+  `time_posted` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `time_modified` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`item_id`),
+  KEY `item_ibfk_1` (`account_id`),
+  CONSTRAINT `item_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `Accounts` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
+
+
+LOCK TABLES `items` WRITE;
+INSERT INTO `items` (item_id, account_id, item_text) VALUES (29,1,'Just a demo task');
+UNLOCK TABLES;
+
 
 -- e.g. "Person", or "Date". 
 -- I think NOT "deadline" etc, that's too specific for this table.
@@ -24,6 +60,7 @@ timeModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 --editableField:
 --e.g. if “person” type, should have a text value. Haven’t entirely thought this one through.
 --iconID: the little image, e.g. from fontawesome
+DROP TABLE IF EXISTS `TagTypes`;
 CREATE TABLE TagTypes (
 id VARCHAR(36) PRIMARY KEY,
 editableField VARCHAR(10), 	
@@ -32,6 +69,7 @@ iconID VARCHAR(10)
 
 --uuid value: for tagging other users etc?
 --description might be e.g. "deadline"
+DROP TABLE IF EXISTS `Tags`;
 CREATE TABLE Tags (
 id VARCHAR(36) PRIMARY KEY,
 tagTypeID VARCHAR(36) references TagTypes(id),
@@ -43,13 +81,14 @@ numericValue3 FLOAT,
 uuidValue VARCHAR(36),				
 timeAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 timeModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-addedBy VARCHAR(36) references Users(id),  
+addedBy VARCHAR(36) references Accounts(account_id),  
 description VARCHAR(100)		 
 );
 
 -- Which items have which tags
+DROP TABLE IF EXISTS `ItemTags`;
 CREATE TABLE ItemTags (
-itemID VARCHAR(36) references Items(id),
+itemID VARCHAR(36) references Items(item_id),
 tagID VARCHAR(36) references Tags(id),
 beginOffset INTEGER,
 endOffset INTEGER,
@@ -58,20 +97,22 @@ primary key (itemID, tagID)
 
 -- Which tags are associated with which other tags for which user
 -- not for MVP
+DROP TABLE IF EXISTS `AssociatedTags`;
 CREATE TABLE AssociatedTags (
 tagID VARCHAR(36) references Tags(id),
 associatedTagID VARCHAR(36) references Tags(id),
-userID VARCHAR(36) references Users(id),
+userID VARCHAR(36) references Accounts(account_id),
 probability REAL,
 primary key (tagID, associatedTagID, userID)
 );
 
 -- Rudimentary "autocorrect" for tags
 -- e.g. ability to apply tag "UNSW" if "uni" is mentioned 
+DROP TABLE IF EXISTS `AssociatedNames`;
 CREATE TABLE AssociatedNames (
 tagID VARCHAR(36) references Tags(id),
 associatedName VARCHAR(100),
-userID VARCHAR(36) references Users(id),
+userID VARCHAR(36) references Accounts(account_id),
 probability REAL,
 primary key (tagID, associatedName, userID)
 );
