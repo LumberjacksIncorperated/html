9e82a6c44f383dd2495790eb0183c70418d35546
Nazif10 <nazif.ahmed12@gmail.com>
2018-09-26 17:22:25 +1000

    uncommented dependencies

--- get_tags.php
+++ get_tags.php
@@ -6,9 +6,9 @@ header("Access-control-allow-origin: *");
 //---------------------------------------- 
 // INCLUDES
 //---------------------------------------- 
-//include_once dirname(__FILE__).'/_dependencies/core_procedures/todolist_php_api.php';
-//include_once dirname(__FILE__).'/_dependencies/php_environment_php_api.php';
-//include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/core_procedures/todolist_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/php_environment_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
 
 //---------------------------------------- 
 // SCRIPT
