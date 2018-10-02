c972c0126782702becbbeb32ee301d0294b19aa4
Daniel Kennedy <dannykennedy@email.com>
2018-10-02 15:28:23 +1000

    dbug

--- get_tags.php
+++ get_tags.php
@@ -20,7 +20,7 @@ include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_p
     }
 
     $todoText = getTodoTextFieldContentsFromCurrentClientRequest();
-    $api_key = getenv('API_KEY');
+    $api_key = $_SERVER['API_KEY'];
 
     echo("yo".$api_key."yo");
 
