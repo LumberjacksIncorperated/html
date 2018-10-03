ec7a500da30ea7c81b5c42c19f15e66792c9804e
Daniel Kennedy <dannykennedy@email.com>
2018-10-03 11:56:17 +1000

    dbug

--- _dependencies/core_procedures/todolist_php_api.php
+++ _dependencies/core_procedures/todolist_php_api.php
@@ -46,7 +46,7 @@ include_once dirname(__FILE__).'/../nlp_functions.php';
 		$tagID = uuidv4(openssl_random_pseudo_bytes(16));
 
 		//function addTag($tagName, $tagType, $tagID)
-		addTag($mytags['entities'], "person", $tagID);
+		addTag($mytags['entities']['name'], "person", $tagID);
 
 		addTagForItem($itemID, $tagID);
 	}
