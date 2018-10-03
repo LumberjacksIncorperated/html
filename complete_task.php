2dd1f12e0e51a5bdac1caf9f1aa4275505227b2f
Daniel Kennedy <dannykennedy@email.com>
2018-10-03 14:44:23 +1000

    mark task completed

--- complete_task.php
+++ complete_task.php
@@ -24,7 +24,7 @@ if (!ensureThisIsASecuredSession()) {
 
 $id_of_tag = getTagIDTextFieldContentsFromCurrentClientRequest();
 
-
+markTaskAsCompleted($id_of_tag);
 
 // $result = getTagsForText($todoText);
 
