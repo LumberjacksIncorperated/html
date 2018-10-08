50559d8fae1ba9396981e06307d5078e10c90502
Nazif10 <nazif.ahmed12@gmail.com>
2018-10-08 15:05:35 +1100

    added date api

--- get_tags.php
+++ get_tags.php
@@ -8,8 +8,8 @@ header("Access-control-allow-origin: *");
 //---------------------------------------- 
 // include_once dirname(__FILE__).'/_dependencies/core_procedures/todolist_php_api.php';
 // include_once dirname(__FILE__).'/_dependencies/php_environment_php_api.php';
-//include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
-//include_once dirname(__FILE__).'/_dependencies/nlp_functions.php';
+include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/nlp_functions.php';
 
 //---------------------------------------- 
 // SCRIPT
@@ -18,21 +18,21 @@ header("Access-control-allow-origin: *");
 //usage: http://165.227.25.45/get_tags.php?todoText=%22hello%20there%20my%20name%20is%20Dan%22
 //erm ideally authenticate as well
 
-//if (!ensureThisIsASecuredSession()) {
-//            echo 'Bad session';
-//}
+if (!ensureThisIsASecuredSession()) {
+            echo 'Bad session';
+}
 
-//$todoText = getTodoTextFieldContentsFromCurrentClientRequest();
+$todoText = getTodoTextFieldContentsFromCurrentClientRequest();
 
-//$text_text = "meeting on the 22nd of October";
+$text_text = "meeting on the 22nd of October"
 
-//$result = getTagsForText($text_text);
+$result = getTagsForText($text_text);
 
-//$result_date = getDateTags($text_text);
+$result_date = getDateTags($text_text);
 
-//echo($result_date);
+echo($result_date);
 
 
-//echo($result);
-    echo("here");
+echo($result);
+
 ?>
