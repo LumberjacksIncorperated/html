144e796f6738a71816fc7e784b65737dadcb2428
Daniel Kennedy <dannykennedy@email.com>
2018-10-08 13:17:20 +1100

    cleanup

---
+++ test_itemid.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// Header needed by REACT
+header("Access-control-allow-origin: *");
+
+//---------------------------------------- 
+// INCLUDES
+//---------------------------------------- 
+include_once dirname(__FILE__).'/_dependencies/core_procedures/todolist_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/php_environment_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
+
+$itemIdToGet = getIdTextFieldContentsFromCurrentClientRequest();
+$result = retrieveSingleItemById($itemIdToGet);
+echo($result);
+
+?>
