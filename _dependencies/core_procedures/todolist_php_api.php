442e4b986f3f92b705dff3b3b3e4eb5517e70a25
Daniel Kennedy <dannykennedy@email.com>
2018-10-09 13:59:45 +1100

    date for task

--- _dependencies/core_procedures/todolist_php_api.php
+++ _dependencies/core_procedures/todolist_php_api.php
@@ -136,9 +136,8 @@ include_once dirname(__FILE__).'/../nlp_functions.php';
 
 	//get all tags for item (we're only returning the tag name and tag type)
 	function getTagsForItem($itemID) {
-		//COALESCE(Tags.textValue, Tags.dateTimeValue) as textValue,
 		$r = fetchMultipleRecordsByMakingSQLQuery("select 
-												   Tags.textValue as textValue, 
+												   COALESCE(Tags.textValue, Tags.dateTimeValue) as textValue, 
 												   TagTypes.name as tagType, Tags.id 
 												   from ItemTags 
 												   JOIN Tags ON Tags.id LIKE ItemTags.tagID 
