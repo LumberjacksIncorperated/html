b2922feee9edc5cfc8478c5d019efea7170dd91d
Daniel Kennedy <dannykennedy@email.com>
2018-10-10 23:26:53 +1100

    search items by tags

--- _dependencies/core_procedures/get_items_by_tag_api.php
+++ _dependencies/core_procedures/get_items_by_tag_api.php
@@ -35,12 +35,13 @@ function getItemsByTags($queryArray, $accountId){
 	//Turn array into a string in the form: 'John', 'Newtown', 'coffee'
 	$queryArrayString = "";
 	foreach ($queryArray as $queryItem) {
-		$queryArrayString = $queryArrayString."'"; echo("$queryArrayString = $queryArrayString.\"'\";");
-		$queryArrayString = $queryArrayString.$queryItem;
-		$queryArrayString = $queryArrayString."'";
-		$queryArrayString = $queryArrayString.","; echo("$queryArrayString = $queryArrayString.\",\";");
-		$queryArrayString = $queryArrayString." "; 
+		$queryArrayString = $queryArrayString."'"; echo($queryArrayString);
+		$queryArrayString = $queryArrayString.$queryItem; echo($queryArrayString);
+		$queryArrayString = $queryArrayString."'"; echo($queryArrayString);
+		$queryArrayString = $queryArrayString.","; echo($queryArrayString);
+		$queryArrayString = $queryArrayString." "; echo($queryArrayString);
 	}
+	echo("-------".$queryArrayString."----------");
 	$queryArrayString = rtrim($queryArrayString,", ");
 	echo(" queryArrayString = ".$queryArrayString);
 
