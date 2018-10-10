792489ae46cee443640650478f43f97652034618
Nazif10 <nazif.ahmed12@gmail.com>
2018-10-10 14:21:12 +1100

    datetime

--- _dependencies/nlp_functions.php
+++ _dependencies/nlp_functions.php
@@ -51,7 +51,7 @@ function getTagsForText($text){
 }
 
 function getDateTags($text){
-	$curlCommandString = "curl -X POST \"https://api.dateparser.io/DMY/parse\" -H \"accept: application/json\" -H \"X-API-KEY: df60929ddd5df0859aceb28d881b67a3dae25d242febf70afd0d51fb280240f1\" -H \"Content-Type: application/json\" -d \"[\\\".$text.\\\"]\"";
+	$curlCommandString = 'curl -X POST "https://api.dateparser.io/DMY/parse" -H "accept: application/json" -H "X-API-KEY: df60929ddd5df0859aceb28d881b67a3dae25d242febf70afd0d51fb280240f1" -H "Content-Type: application/json" -d "[\\".$text.\\"]"';
 	echo("$curlCommandString");
     $output = shell_exec("$curlCommandString");
     echo("333");
@@ -61,13 +61,13 @@ function getDateTags($text){
     //echo("2");
     //echo(($output[0][0]);
    // echo("2");
-    echo($tagDate);
+    echo($output);
    // echo("2");
   //  echo($tagDate["date"]);
     echo("333");
 	return $output;
 
-[[{"date":"2018-11-20T00:00:00.000Z","ms":1542672000000,"epoch":1542672000,"match":"20th of November","index":16}]]
+//[[{"date":"2018-11-20T00:00:00.000Z","ms":1542672000000,"epoch":1542672000,"match":"20th of November","index":16}]]
 
 	/*
 echo("2");
