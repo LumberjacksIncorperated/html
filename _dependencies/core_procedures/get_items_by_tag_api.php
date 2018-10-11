d82ab27563b54235ddcd2a8c6bbf04cd80197e87
Daniel Kennedy <dannykennedy@email.com>
2018-10-11 13:21:15 +1100

    search items by tags

--- _dependencies/core_procedures/get_items_by_tag_api.php
+++ _dependencies/core_procedures/get_items_by_tag_api.php
@@ -52,7 +52,17 @@ function getItemsByTags($queryArray, $accountId){
 
 	$mystring = $queryArray[0];
 
-	modifyDataByMakingSQLQuery("CREATE OR REPLACE VIEW cats as SELECT * from items where account_id = 2;");
+	modifyDataByMakingSQLQuery("CREATE OR REPLACE VIEW user_items as SELECT * from items where account_id = $accountId;");
+	modifyDataByMakingSQLQuery("CREATE OR REPLACE VIEW items_with_the_tags as 
+								SELECT * from user_items 
+								JOIN ItemTags ON user_items.item_id LIKE ItemTags.itemID
+								JOIN Tags ON Tags.id LIKE ItemTags.tagID
+								WHERE Tags.textValue IN ('park');");
+
+	modifyDataByMakingSQLQuery("CREATE OR REPLACE VIEW items_with_tag_count as
+								SELECT item_id, count(*) as match_count
+								FROM items_with_the_tags
+								GROUP BY item_id;");
 
 	$r = fetchMultipleRecordsByMakingSQLQuery(
 
@@ -70,16 +80,16 @@ function getItemsByTags($queryArray, $accountId){
 //
 // CREATE OR REPLACE VIEW user_items as SELECT * from items where account_id = 2;
 
-// 	CREATE OR REPLACE VIEW items_with_the_tags as 
-// 	SELECT * from user_items 
-// 	JOIN ItemTags ON user_items.item_id LIKE ItemTags.itemID
-// 	JOIN Tags ON Tags.id LIKE ItemTags.tagID
-// 	WHERE Tags.textValue IN ('coffee');
+	// CREATE OR REPLACE VIEW items_with_the_tags as 
+	// SELECT * from user_items 
+	// JOIN ItemTags ON user_items.item_id LIKE ItemTags.itemID
+	// JOIN Tags ON Tags.id LIKE ItemTags.tagID
+	// WHERE Tags.textValue IN ('coffee');
 
-// 	CREATE OR REPLACE VIEW items_with_tag_count as
-// 	SELECT item_id, count(*) as match_count
-// 	FROM items_with_the_tags
-// 	GROUP BY item_id;
+	// CREATE OR REPLACE VIEW items_with_tag_count as
+	// SELECT item_id, count(*) as match_count
+	// FROM items_with_the_tags
+	// GROUP BY item_id;
 
 
 
