b4477fffabab79df2cb8d1c4514c0b0bb513edc8
Daniel Kennedy <dannykennedy@email.com>
2018-10-14 15:20:18 +1100

    add real dates to filter tags

--- get_tag_by_name.php
+++ get_tag_by_name.php
@@ -28,18 +28,21 @@ include_once dirname(__FILE__).'/_dependencies/core_procedures/tags_api.php';
 // INTERNAL FUNCTIONS
 //---------------------------------------- 
 
-    function _createTagListArrayFromQueryResults($queryArray, $tagText){
+    function _createTagListArrayFromQueryResults($queryArray, $datesArray, $tagText){
         
         $returnArray = array();
 
         // If nothing was found
-        if ($queryArray == NULL){
+        if (($queryArray == NULL) and ($datesArray == NULL)){
             array_push($returnArray, array("textValue" => $tagText, "tagID" => NULL, "tagType" => NULL));
         }
         // If something was found
         else {
             foreach ($queryArray as $queryResult) {
                 array_push($returnArray, array("textValue" => $tagText, "tagID" => $queryResult['tagId'], "tagType" => $queryResult['tagType']));
+            }
+            foreach ($datesArray as $dateResult) {
+                array_push($returnArray, array("textValue" => $dateResult, "tagID" => "id-here", "tagType" => "date"));
             }
         }
         return $returnArray;
@@ -68,14 +71,14 @@ include_once dirname(__FILE__).'/_dependencies/core_procedures/tags_api.php';
     // Query the DB
     $tagsArray = getTagsByNameAndUser($tagText, $userId); 
 
-    $allTagsArray = array();
+    // $allTagsArray = array();
 
     // array_push($allTagsArray, $datesArray);
-    array_push($allTagsArray, $tagsArray);
+    // array_push($allTagsArray, $tagsArray);
 
 
     // Make sure that the FE gets a predictable array type, not just null, even if DB query result is empty
-    $prettyArrayWithNullValues = _createTagListArrayFromQueryResults($allTagsArray, $tagText);
+    $prettyArrayWithNullValues = _createTagListArrayFromQueryResults($tagsArray, $datesArray, $tagText);
 
     // Echo the array as JSON
     _displayArrayAsJson($prettyArrayWithNullValues);
