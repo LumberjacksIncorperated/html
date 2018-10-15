23429d3670523f5c2a9be3cf3341fa42c117f130
Nazif10 <nazif.ahmed12@gmail.com>
2018-10-15 14:46:38 +1100

    sign up

---
+++ signup.php
@@ -0,0 +1,41 @@
+<?php
+//--------------------------------------------------------------------------------------------------------------
+//
+// PURPOSE
+// -------
+// To login:
+//
+// AUTHOR
+// -------
+// Lumberjacks Incorperated (2018)
+//--------------------------------------------------------------------------------------------------------------
+
+// Header needed for react
+header("Access-control-allow-origin: *");
+
+//---------------------------------------- 
+// INCLUDES
+//---------------------------------------- 
+include_once dirname(__FILE__).'/_dependencies/php_environment_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/core_procedures/secured_session_php_api.php';
+include_once dirname(__FILE__).'/_dependencies/core_procedures/todolist_php_api.php';
+
+
+//---------------------------------------- 
+// SCRIPT
+//---------------------------------------- 
+	$username = getUsernameFieldContentsFromCurrentClientRequest();
+	$password = getPasswordFieldContentsFromCurrentClientRequest();
+	$email = getEmailFieldContentFromCurrentClientRequest();
+	$firstName = getFirstNameFieldContentFromCurrentClientRequest();
+	$lastName = getLastNameFieldContentFromCurrentClientRequest();
+
+	// $loginSessionKey = getSessionKeyForNewSessionWithUsernameAndPassword($username, $password);
+	// if ($loginSessionKey) {
+	// 	echo $loginSessionKey;
+	// } else {
+	// 	echo 'Failed to login, incorrect username and password';
+	// }
+
+	createNewUserAccount($username,$password,$email,$firstName,$lastName);
+?>
