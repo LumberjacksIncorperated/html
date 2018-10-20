39d922197a4d7239d56f27bbfd0a15a65916362e
Daniel Kennedy <dannykennedy@email.com>
2018-10-20 21:56:59 +1100

    custom date tagging

--- _dependencies/core_procedures/todolist_php_api.php
+++ _dependencies/core_procedures/todolist_php_api.php
@@ -232,7 +232,7 @@ include_once dirname(__FILE__).'/../nlp_functions.php';
 
 		// Hackily make sure that we don't double up on dates found
 		foreach ($allDates as $dateString) {
-			if (count(preg_grep($dateString, $allDates)) > 1){
+			if (count(preg_grep($dateString, $allDates)) > 0){
 				unset($dateString);
 			} else {
 				$tagID = uuidv4(openssl_random_pseudo_bytes(16));
