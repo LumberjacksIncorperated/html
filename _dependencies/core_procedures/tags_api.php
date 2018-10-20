20041578bb5639e83f8cb44896c2e63e25c912b0
Daniel Kennedy <dannykennedy@email.com>
2018-10-20 15:11:43 +1100

    update date tags properly

--- _dependencies/core_procedures/tags_api.php
+++ _dependencies/core_procedures/tags_api.php
@@ -57,6 +57,12 @@ function deleteAllTagsForItem($itemID){
 // Update tag id manually on site
 function updateDateTag($tagId, $dateString){
 
+    if (preg_match("/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}/", $dateString)){
+        echo "****** valid ********";
+    } else {
+        echo "****** invalid ********";
+    }
+
     $dateValue = date('Y-m-d', strtotime($dateString));
     $result = modifyDataByMakingSQLQuery("UPDATE Tags SET dateTimeValue = \"$dateValue\" WHERE id LIKE \"$tagId\";");
 
