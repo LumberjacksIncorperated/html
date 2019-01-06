d73190833a4551f3081c61edf0e8e7979449f8b2
Daniel Kennedy <dannykennedy@email.com>
2019-01-06 15:00:39 -0500

    top tags

--- get_top_tags_for_user.php
+++ get_top_tags_for_user.php
@@ -44,8 +44,9 @@ include_once dirname(__FILE__).'/_dependencies/core_procedures/charts_api.php';
     }
 
     //Get top tags for user
-    $topTags = getTopSixTagsForUser($user_id);
-
+    $topTags = [];
+    array_push($topTags, getTopSixTagsForUser($user_id));
+    
     echo (json_encode($topTags));
 
 ?>
